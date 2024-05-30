f2a46681195d7c0a6da20fb75e1a52143dbc5b5d
Rafael <Rafael.mattos468@gmail.com>
2024-05-29 21:48:56 -0300

    refactor: separando a api do componente que vai consumi-la

--- src/app/components/ola.jsx
+++ src/app/components/ola.jsx
@@ -1,19 +1,8 @@
+
+
 import React from "react"
+import { getData } from '../services/api'
 
-async function getData() {
-    const res = await fetch('https://brasilapi.com.br/api/feriados/v1/2025')
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
-
-    if (!res.ok) {
-        // This will activate the closest `error.js` Error Boundary
-
-        throw new Error('Failed to fetch data')
-    }
-
-
-    return res.json()
-}
 
 export default async function Ola() {
     const dados = await getData()
---
+++ src/app/services/api.js
@@ -0,0 +1,18 @@
+'use server'
+export default async function getData() {
+   
+    const res = await fetch('https://brasilapi.com.br/api/feriados/v1')
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+
+        throw new Error('Failed to fetch data')
+    }
+
+
+    return res.json()
+}
+
+ 
